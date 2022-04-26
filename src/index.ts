afbbe1437e46e2b6438addb2e19d707779a19b1b
david <david.gonzalez@gradiweb.com>
2022-04-25 20:00:56 -0500

    [fix] bugs middleware

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,27 @@
 import express from 'express'
 import routerApi from './routes'
 import { logErrors, errorHandler, boomErrorHandler } from './middleware/error.handler'
+import cors from 'cors';
 
 const app = express()
 const port = 5000
+
 app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+
+const options = {
+  origin: (origin: any, callback: any) => {
+    if (whitelist.includes(origin) != origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+
+app.use(cors());
+
 routerApi(app);
 
 app.use(logErrors)
--- src/middleware/validation.handler.ts
+++ src/middleware/validation.handler.ts
@@ -1,9 +1,30 @@
 import { Request, Response, NextFunction } from 'express';
 import boom  from '@hapi/boom'
 
-export function ValidatorHandler (schema: any, property: any){
+export function ValidatorHandler (schema: any, property: string){
  return (req: Request, res: Response, next: NextFunction) =>{
-    const data = req[property];
+  
+  let data: any = []
+    
+    switch(property) { 
+      case 'params': { 
+        data = req.params;
+         break; 
+      } 
+      case 'body': { 
+        data = req.body;
+         break; 
+      } 
+      case 'query': { 
+        data = req.query;
+        break; 
+      } 
+      default: { 
+        data = '';
+        break; 
+      } 
+   } 
+
     const { error } = schema.validate(data, { abortEarly: false });
     if (error) {
       next(boom.badRequest(error));
